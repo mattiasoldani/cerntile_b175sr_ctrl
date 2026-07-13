c896eafdedb7223b0a6e2397bb03db036799fd38
Mattia Soldani <mattiasoldani93@gmail.com>
2026-07-13 11:47:33 +0200

    introducing commands to move motors

--- cerntile_b175sr_ctrl.ino
+++ cerntile_b175sr_ctrl.ino
@@ -1,22 +1,22 @@
 // master, motor
-#define P0_ENA 1
-#define P0_PUL 1
-#define P0_DIR 1
+#define P0_ENA 4
+#define P0_PUL 3
+#define P0_DIR 2
 
 // slave, motor
-#define P1_ENA 1
-#define P1_PUL 1
-#define P1_DIR 1
+#define P1_ENA 7
+#define P1_PUL 6
+#define P1_DIR 5
 
 // master, stoppers
-#define P0_SBAS 13
-#define P0_SSIG_L 1
-#define P0_SSIG_R 1
+#define P0_SBAS 10
+#define P0_SSIG_L 9
+#define P0_SSIG_R 8
 
 // slave, stoppers
-#define P1_SBAS 10
-#define P1_SSIG_L 1
-#define P1_SSIG_R 1
+#define P1_SBAS 13
+#define P1_SSIG_L 12
+#define P1_SSIG_R 11
 
 ////////////////////////////////////////////////////////////////////////////
 // dependencies ////////////////////////////////////////////////////////////
@@ -69,5 +69,32 @@ void loop() {
   // at the start of each loop, check whether any stopper is pressed
   bool b_S0l=0, b_S0r=0, b_S1l=0, b_S1r=0, b_Sor=0;
   b_Sor = get_stoppers(b_S0l, b_S0r, b_S1l, b_S1r);
+
+  for (int i=0; i<500; i++) { // forward motion of both motors at once
+    if(b_Sor){continue;}
+    digitalWrite(P0_DIR, LOW);
+    digitalWrite(P1_DIR, LOW);
+    digitalWrite(P0_ENA, HIGH);
+    digitalWrite(P1_ENA, HIGH);
+    digitalWrite(P0_PUL, HIGH);
+    digitalWrite(P1_PUL, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(P0_PUL, LOW);
+    digitalWrite(P1_PUL, LOW);
+    delayMicroseconds(50);
+  }
+  for (int i=0; i<500; i++) { // backward motion of both motors at once
+    if(b_Sor){continue;}
+    digitalWrite(P0_DIR, HIGH);
+    digitalWrite(P1_DIR, HIGH);
+    digitalWrite(P0_ENA, HIGH);
+    digitalWrite(P1_ENA, HIGH);
+    digitalWrite(P0_PUL, HIGH);
+    digitalWrite(P1_PUL, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(P0_PUL, LOW);
+    digitalWrite(P1_PUL, LOW);
+    delayMicroseconds(50);
+  }
 
 }
